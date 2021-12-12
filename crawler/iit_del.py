1d1e200032582a0945c13d739afe9d6da297aa4e
Sourav Das <iamsouravdaswade420@gmail.com>
2021-12-12 09:10:23 +0530

    comment added

--- crawler/iit_del.py
+++ crawler/iit_del.py
@@ -12,7 +12,7 @@ class Delhi:
         self.names_dic = {}
         self.profinfo = []
 
-    def db_insert(self, name, desc):
+    def db_insert(self, name, desc): # inserting into database
         mycursor = mydb.cursor()
         sql = "INSERT INTO iit_delhi (id,name,description,google_scholar) VALUES (NULL,'%s','%s',NULL);" % (name, desc)
         print(sql)
@@ -20,35 +20,35 @@ class Delhi:
         mydb.commit()
         print(mycursor.rowcount, "record inserted.")
 
-    def scrap_info(self):
+    def scrap_info(self): # scraping the data from the website
         r = rq.get(self.scrap_url, verify=False)
         soup = bs(r.content, "html.parser")
-        data = soup.select('.content .clearfix table td')
+        data = soup.select('.content .clearfix table td') # selecting the data from the website
         for x in data:
-            self.names.append(x.text.strip())
+            self.names.append(x.text.strip()) # names of the professors
 
-        start_index = self.names.index('Deptt.of Appl.Mech.')
-        end_index = self.names.index('Prof. Aditeshwar Seth')
+        start_index = self.names.index('Deptt.of Appl.Mech.') # starting index of the data
+        end_index = self.names.index('Prof. Aditeshwar Seth')   # ending index of the data
 
         for prof in range(start_index, end_index + 1):
 
             if "Dr. " in self.names[prof]:
-                removed = self.names[prof].replace("Dr.", "")
-                self.removed_names.append(removed)
+                removed = self.names[prof].replace("Dr.", "") # removing the Dr. from the names
+                self.removed_names.append(removed) # appending the names to the list
 
             elif "Prof. " in self.names[prof]:
-                removed = self.names[prof].replace("Prof.", "")
-                self.removed_names.append(removed)
+                removed = self.names[prof].replace("Prof.", "") # removing the Prof. from the names
+                self.removed_names.append(removed) # appending the names to the list
 
             else:
-                self.removed_names.append(self.names[prof])
+                self.removed_names.append(self.names[prof]) # appending the names to the list
 
         print(len(self.removed_names))
 
         # making the names to dictionary and appending to new list
 
         for x in range(len(self.removed_names) - 1):
-            self.names_dic = {"desc": self.removed_names[x], "name": self.removed_names[x + 1]}
+            self.names_dic = {"desc": self.removed_names[x], "name": self.removed_names[x + 1]} # making the dictionary
             self.profinfo.append(self.names_dic)
 
         for prof in self.profinfo:
