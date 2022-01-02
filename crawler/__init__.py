68920f922fbc0dd5c44f9d72b823659c35eba224
Sourav Das <iamsouravdaswade420@gmail.com>
2022-01-02 17:48:33 +0530

    nit kurukshetra gs

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -6,4 +6,10 @@
 # myresult=mycur.fetchall()
 # for row in myresult:
 #     print(row[0])
-print("Mango is good")
+# print("Mango is good")
+try:
+    print(x)
+# except NameError:
+#   print("Variable x is not defined")
+except:
+  print("Something else went wrong")
--- crawler/nit_kurks.py
+++ crawler/nit_kurks.py
@@ -8,18 +8,36 @@ from DB_CON import mydb
 class NitKuruk:
     def __init__(self):
         self.url = "http://www.nitkkr.ac.in/sub_courses.php?id=24&id3=35"
+        self.names = []
+
+    def db_save(self, name):
+
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO nit_kuruk VALUES (NULL,'%s',NULL)" % (name)
+        mycursor.execute(sql)
+        mydb.commit()
+        print(mycursor.rowcount, "record inserted.")
 
     def scrap(self):
         req = rq.get(self.url)
         soup = bs(req.text, "html.parser")
-        table = soup.select(".table-responsive table")
-        i=1
-        for name in table:
-            prop=name.text.strip()
-            if prop!="" and prop!="\n":
-                print(i,prop)
-                i+=1
+
+        try:
+            for i in range(1, 11):
+                selctor = "#main-content > div > div.col-md-12 > div > div > table > tbody > tr:nth-child(" + str(
+                    i) + ") > td:nth-child(3)"
+
+                name = soup.select(selctor)
+                self.names.append(name)
+
+            for name in self.names:
+                self.db_save(name[0].text.strip())
+
+        except AttributeError:
+            print("ERROR")
 
 
 ob = NitKuruk()
 ob.scrap()
+
+# ALTER TABLE table AUTO_INCREMENT = 1;
